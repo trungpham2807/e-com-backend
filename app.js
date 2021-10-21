40c354830efe955885ba541ed86a9bc4fdbe8ab8
luke <lukehoang195@gmail.com>
2021-10-21 11:39:41 +0700

    Error handler

--- app.js
+++ app.js
@@ -16,4 +16,21 @@ app.use(cors());
 
 app.use("/api", indexRouter);
 
+/** when request match no ruote, create error */
+app.use((req, res, next) => {
+  const error = new Error("Wrong url");
+  error.statusCode = 404;
+  next(error);
+});
+
+/** when next(error) called,
+ * this function will send error message */
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    return res.status(err.status).send(err.message);
+  } else {
+    return res.status(500).send(err.message);
+  }
+});
+
 module.exports = app;
